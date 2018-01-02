a65439705601b26bd0616b78afb7da4aed279bae
Alex Seif <alex.seif@gmail.com>
2018-01-03 00:55:59 +0200

    adding images

--- alx_infinite_scroll/alx-infinite-scroll.js
+++ alx_infinite_scroll/alx-infinite-scroll.js
@@ -4,14 +4,14 @@
 
 
 (function ($) {
-  Drupal.behaviors.alxInfiniteScroll = {
-    attach: function(){
-      $('#content').cleverInfiniteScroll({
-        contentsWrapperSelector: '#content',
-        contentSelector: '.content',
-        nextSelector: '.flippy .next a',
-        loadImage: ''
-      });
+    Drupal.behaviors.alxInfiniteScroll = {
+        attach: function () {
+            $('.side_content').cleverInfiniteScroll({
+                contentsWrapperSelector: '.side_content',
+                contentSelector: '.node-news-article',
+                nextSelector: '.flippy .next a',
+                loadImage: drupal_get_path('module', 'alx_infinite_scroll') + '/images/ajax-loader.gif'
+            });
+        }
     }
-  }
 })(jQuery);
